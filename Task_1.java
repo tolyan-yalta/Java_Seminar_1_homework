fdb8f64326db054279938c700ab2ab081feca062
anatoly <tolyan.yalta@yandex.ru>
2022-12-07 15:52:19 +0300

    Changed Task_1.java

--- Task_1.java
+++ Task_1.java
@@ -4,17 +4,13 @@ import java.util.Scanner;
 
 public class Task_1 {
     public static void main(String[] args) {
-        System.out.println("Введите число N: ");
+        System.out.println("Введите число n: ");
         Scanner scanner = new Scanner((System.in));
         Integer n = scanner.nextInt();
 
         int number;
         number = n *(n + 1) / 2;
 
-        //String res = f("Для n = %s треугольное число будет равно: ", n);
-
         System.out.printf("Для n = %d треугольное число будет равно: %d", n, number);
-        //System.out.println(number);
     }
-
 }
